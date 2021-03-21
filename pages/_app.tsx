1127d6b425b7c18f423f395fdedc584884fd8974
erickArita <erick.emao@gmail.com>
2021-03-21 00:13:57 -0600

    smoot parallax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
+import { ParallaxProvider } from 'react-scroll-parallax'
+
 import 'normalize.css'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
- 
+
 type ss = {
   Component: FC<any>
   pageProps: any[]
@@ -10,7 +12,9 @@ type ss = {
 export default function MyApp({ Component, pageProps }: ss) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
     </Layout>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Header from '../components/Header'
-import { ParallaxProvider } from 'react-scroll-parallax'
 function IndexPage() {
   return (
-    <ParallaxProvider >
-      <Header/>
-    </ParallaxProvider>
+    <Header />
   )
 }
 
