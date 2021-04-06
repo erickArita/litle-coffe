85616a2c28b2df6ea19533f39a5da98f5b004ff2
erickArita <erick.emao@gmail.com>
2021-04-06 16:15:49 -0600

    images special optimizations

--- utils/specials.ts
+++ utils/specials.ts
@@ -8,25 +8,25 @@ export const specialCard: SpecialCard[] = [
   {
     title: 'Tacos al pastor',
     text: 'Un delicioso trio de tacos que te harán agua la boca',
-    img: '/food/food.jpg',
+    img: '/food/food.webp',
     price: '50.00',
   },
   {
     title: 'Desayuno',
     text: 'Preparado para alegrar tu día',
-    img: '/food/header.jpg',
+    img: '/food/header.webp',
     price: '60.00',
   },
   {
     title: 'Nachos',
     text: 'Una explosion de sabor en tu boca',
-    img: '/food/nachos.jpg',
+    img: '/food/nachos.webp',
     price: '60.00',
   },
   {
     title: 'Micheladas',
     text: 'Todos los jueves una bebida al 2x1 ven a disfrutar ',
-    img: '/food/micheladas.jpg',
+    img: '/food/micheladas.webp',
     price: '25.00',
   }
 ]
